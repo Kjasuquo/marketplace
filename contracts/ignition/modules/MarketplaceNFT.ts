ca5c823d7fc2a0a6d40d273aee4cf7181a85371a
Kjasuquo <okoasuquo@yahoo.com>
2025-11-11 14:41:36 +0100

    contract removed from frontened repo

--- contracts/ignition/modules/MarketplaceNFT.ts
+++
@@ -1,8 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("MarketplaceNFTModule", (m) => {
-  // Deploy the MarketplaceNFT contract (no constructor args)
-  const marketplaceNFT = m.contract("MarketplaceNFT");
-
-  return { marketplaceNFT };
-});
